393ed2af5e74f38b336ee76a5104c63003d23c51
Bryanna Gavino <bry_gavino01@berkeley.edu>
2021-01-24 00:23:03 -0800

    homepage mixtape buttons

--- index.py
+++ index.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/filter/")
+def filter():
+    return render_template("filter.html")
+
 if __name__ == "__name__":
     app.run()
